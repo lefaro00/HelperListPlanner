dd6ca017fd25780b6d5a11a798a03c5f181464b0
lefaro00 <lefaro00@gmail.com>
2022-07-28 18:18:02 +0200

    Added Host to Event and stuff to event calendar

--- HelperListPlanner/Data/Controlling/EventCalender.cs
+++ HelperListPlanner/Data/Controlling/EventCalender.cs
@@ -3,17 +3,52 @@ namespace HelperListPlanner.Data.Controlling
 {
     public static class EventCalender
     {
-        public static List<Event> OpenEvents { get; set; } = new();
-        public static List<Entities.Host> Hosts { get; } = new();
-
-        public static void AddHost(Entities.Host host)
-        {
-            Hosts.Add(host);
+        public static List<Event> OpenEvents { get; } = new();
+        public static List<Event> PassedEvents { get; } = new();
+        public static IEnumerable<Entities.Host> Hosts{ get 
+           {
+                List<Entities.Host> hosts = new();
+                foreach (var _event in OpenEvents)
+                {
+                    hosts.Add(_event.Host);
+                }
+                foreach (var _event in PassedEvents)
+                {
+                    hosts.Add(_event.Host);
+                }
+                hosts = (List<Entities.Host>)hosts.Distinct();
+                return hosts;
+            }
         }
-
-        public static void EnterEvent(Entities.Host host, Event @event)
+        private static IEnumerable<Event> Events { get
+            {
+                List<Event> events = new();
+                events.AddRange(PassedEvents);
+                events.AddRange(OpenEvents);
+                return events;
+            }
+        }
+        public static void EnterEvent(Event @event)
         {
-            
+            if(@event.StartTime < DateTime.Now)
+            {
+                OpenEvents.Add(@event);
+            }
+            else
+            {
+                PassedEvents.Add(@event);
+            }
+        }
+        public static void DeleteEvent(Event _event, bool markAsCanceled)
+        {
+            if (markAsCanceled)
+            {
+                var success = OpenEvents.Remove(_event);
+                if (!success)
+                {
+                    PassedEvents.Remove(_event);
+                }
+            }
         }
     }
 }
--- HelperListPlanner/Data/Entities/Event.cs
+++ HelperListPlanner/Data/Entities/Event.cs
@@ -3,15 +3,17 @@
     public class Event
     {
         string Name { get; set; }
+        public Entities.Host Host { get; }
         string Description { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         List<Shift> Shifts { get; set; }
         Location Location { get; set; }
 
-        public Event(string name, string description, DateTime startTime, DateTime endTime, List<Shift> shifts, Location location)
+        public Event(string name, Entities.Host host, string description, DateTime startTime, DateTime endTime, List<Shift> shifts, Location location)
         {
             Name = name;
+            this.Host = host;
             Description = description;
             StartTime = startTime;
             EndTime = endTime;
